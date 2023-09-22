3bc60b79482e8a5f12fb346e8299a89378d9701c
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-09-22 20:38:51 +0800

    sm8550-common: Compile HWUI for performance
    * Activates a few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,9 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 BOARD_INIT_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
