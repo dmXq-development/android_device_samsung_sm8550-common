2c3386b02f6a664d24a109d7ec4e8c2fa8e72ce8
Tim Zimmermann <tim@linux4.de>
2024-07-30 05:57:11 +0200

    sm8550-common: Switch to hardware/samsung FCM
    Change-Id: Id915542a0b5c4d27e488cae0d97ed36d299a428b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,7 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/vintf/device_framework_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE :=  \
